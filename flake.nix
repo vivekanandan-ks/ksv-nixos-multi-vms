0ab450e8fd839bffa653043b5a53f0bfb809f22f
vivekanandan-ks <ksvdevksv@gmail.com>
2025-05-11 18:37:09 +0530

    First Commit

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Multiple NixOS VMs with SSH access";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      
+      # Define a function to create each VM
+      createVM = vmName: hport: gport: nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ({ pkgs, ... }: {
+            imports = [ "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
+
+            networking.hostName = vmName;
+            networking.firewall.enable = false;
+
+            services.openssh.enable = true;
+            services.openssh.settings.PermitRootLogin = "yes";
+            users.users.root = {
+              isNormalUser = false;
+              hashedPassword = "$6$IuOZIMCx7ZwxSygV$xolfZMDt1h3mtxlzTuBdLqeW2BnkSo12c4yyy.Skt2qlOpJAPxZmYw1XlRE1wqluv0imrPKbRuKRuN78LJRrn0";
+            };
+
+            virtualisation.forwardPorts = [
+              {
+                from = "host";
+                host.port = hport;
+                guest.port = gport;
+              }
+            ];
+
+            environment.systemPackages = with pkgs; [ btop ];
+            system.stateVersion = "24.11";
+          })
+        ];
+      };
+    in
+    {
+      # NixOS configurations
+      nixosConfigurations = {
+        vm1 = createVM "vm1" 2222 2222;
+        vm2 = createVM "vm2" 3333 3333;
+        vm3 = createVM "vm3" 4444 4444;
+      };
+      
+      # Expose the VMs as buildable packages
+      packages.${system} = {
+        vm1 = self.nixosConfigurations.vm1.config.system.build.vm;
+        vm2 = self.nixosConfigurations.vm2.config.system.build.vm;
+        vm3 = self.nixosConfigurations.vm3.config.system.build.vm;
+        default = self.nixosConfigurations.vm1.config.system.build.vm;
+      };
+    };
+}
